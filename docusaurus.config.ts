78e35b23d2f0929b38573d68f457bb2d2a28516f
xinhanyang <xinhanyang061@gmail.com>
2024-09-15 20:55:44 +0800

    test

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -2,4 +2,6 @@ export default {
   url: 'https://docs.netlify.app', // Url to your site with no trailing slash
   baseUrl: '/', // Base directory of your site relative to your repo
   // ...
+  title: 'docs',
+  // other configuration options
 };
